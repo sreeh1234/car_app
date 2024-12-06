1e8ab60f2c02809abc967d7c13bed391b8dcfa10
sreeh1234 <sreehariapzzz@gmail.com>
2024-12-06 17:55:18 +0530

    car app

--- car_com/app/urls.py
+++ car_com/app/urls.py
@@ -4,6 +4,13 @@ from . import views
 
 urlpatterns=[
      path('',views.user_login),
-    # path('shop_home',views.shop_home),
+    path('shop_home',views.shop_home),
     path('logout',views.user_logout),
+    
+    
+    
+    
+    
+     path('register',views.register),
+     path('user_home',views.user_home),
 ]
--- car_com/app/views.py
+++ car_com/app/views.py
@@ -6,23 +6,55 @@ import os
 from django.contrib.auth.models import User
 
 # Create your views here.
+
+
 def user_login(req):
+    if 'shop' in req.session:
+        return redirect(shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
     if req.method=='POST':
         uname=req.POST['uname']
         password=req.POST['password']
-        user=authenticate(username=uname,password=password)
-        if user:
-            login(req,user)
-            return redirect(user_login)
+        shop=authenticate(username=uname,password=password)
+        if shop:
+            login(req,shop)
+            if shop.is_superuser:
+                
+                req.session['shop']=uname       #create
+                return redirect(shop_home)
+            else:
+                req.session['user']=uname
+                return redirect(user_home)
         else:
             messages.warning(req,'invalid username or password')
             return redirect(user_login)
-    return render(req,'login.html')
+    else:
+        return render(req,'login.html')
 
 def user_logout(req):
     logout(req)
     req.session.flush()        
     return redirect(user_login)
+
+
+
+
+
+
+
+def shop_home(req):
+    # if 'shop' in req.session:
+    #     products=product.objects.all()
+    return render(req,'shop/home.html')
+    # else:
+        # return redirect(user_login)
+
+
+
+
+# -----------------------------------user----------------------------------------
+# -------------------------------------------------------------------------------
 
 
 def register(req):
@@ -38,4 +70,13 @@ def register(req):
             messages.warning(req,'Email Already Exist')
             return redirect(register)
     else:
-        return render(req,'register.html')
+        return render(req,'user/register.html')
+
+
+
+def user_home(req):
+    # if 'user' in req.session:
+    #     products=product.objects.all()
+    return render(req,'user/home.html')
+    # else:
+    #     return redirect(user_login)
